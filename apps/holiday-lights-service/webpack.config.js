45339a21eb0cfc186864bcbafc137658f9182c7d
novarx <info@novarx.ch>
2025-12-15 08:33:19 +0100

    fix config

--- apps/holiday-lights-service/webpack.config.js
+++ apps/holiday-lights-service/webpack.config.js
@@ -2,6 +2,18 @@ const { join, resolve } = require('path');
 const nodeExternals = require('webpack-node-externals');
 const CopyPlugin = require('copy-webpack-plugin');
 
+// Lazy-loaded modules that NestJS tries to require optionally
+const lazyImports = [
+  '@nestjs/microservices',
+  '@nestjs/microservices/microservices-module',
+  '@nestjs/websockets',
+  '@nestjs/websockets/socket-module',
+  '@nestjs/platform-socket.io',
+  'cache-manager',
+  'class-validator',
+  'class-transformer',
+];
+
 module.exports = {
   entry: './src/main.ts',
   target: 'node',
@@ -35,17 +47,44 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.node$/,
+        use: 'node-loader',
+      },
     ],
   },
-  externals: [nodeExternals()],
+  externals: [
+    nodeExternals({
+      modulesFromFile: true,
+      allowlist: [/@holiday-lights\/imager-core/],
+    }),
+    'rpi-led-matrix',
+  ],
   plugins: [
     new CopyPlugin({
       patterns: [
         { from: 'src/assets', to: 'assets', noErrorOnMissing: true },
       ],
     }),
+    {
+      apply: (compiler) => {
+        compiler.hooks.normalModuleFactory.tap('IgnorePlugin', (factory) => {
+          factory.hooks.beforeResolve.tap('IgnorePlugin', (resolveData) => {
+            if (lazyImports.includes(resolveData.request)) {
+              return false;
+            }
+          });
+        });
+      },
+    },
   ],
   optimization: {
     minimize: false,
   },
+  ignoreWarnings: [
+    {
+      module: /node_modules/,
+      message: /Can't resolve/,
+    },
+  ],
 };
